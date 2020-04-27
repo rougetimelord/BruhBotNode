ca811751a4bf8dcdbb2e65b2e0d4e126730be8d1
Rouge <code@r0uge.org>
2020-04-27 16:10:52 -0700

    Fix indent

--- server.js
+++ server.js
@@ -8,82 +8,86 @@ const setRegex = /^!set/i;
 const testRegex = /^!test/i;
 
 let key_gen = (guild) => {
-  return new Promise((resolve, reject) => {
-    if (guild.id) {
-      resolve(`${guild.id}_channel`);
-    } else {
-      reject(`Guild has no id`);
-    }
-  });
+    return new Promise((resolve, reject) => {
+        if (guild.id) {
+            resolve(`${guild.id}_channel`);
+        } else {
+            reject(`Guild has no id`);
+        }
+    });
 };
 
 let json_dump = () => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(
-      "./data.json",
-      JSON.stringify(data, null, 4),
-      (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      }
-    );
-  });
+    return new Promise((resolve, reject) => {
+        fs.writeFile(
+            "./data.json",
+            JSON.stringify(data, null, 4),
+            (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            }
+        );
+    });
 };
 
 client.once("ready", () => {
-  console.log(
-    `Logged in as: ${client.user.username} with id: ${client.user.id}`
-  );
+    console.log(
+        `Logged in as: ${client.user.username} with id: ${client.user.id}`
+    );
 });
 
 let send_message = async (member) => {
-  key_gen(member.guild).then(async (key) => {
-    if (key in data) {
-      await client.channels
-        .fetch(data[key])
-        .send(Math.floor(Math.random() * 2) == 0 ? "bruh" : "Bruh");
-    } else {
-      throw new Error(`No channel ID set in server ${member.guild.name}`);
-    }
-  });
+    console.log();
+    key_gen(member.guild).then(async (key) => {
+        if (key in data) {
+            await client.channels
+                .fetch(data[key])
+                .send(Math.floor(Math.random() * 2) == 0 ? "bruh" : "Bruh");
+        } else {
+            throw new Error(`No channel ID set in server ${member.guild.name}`);
+        }
+    });
 };
 
 client.on("message", async (message) => {
-  key_gen(message.guild)
-    .then(
-      async (key) => {
-        if (message.member.hasPermission("MANAGE_CHANNELS")) {
-          if (
-            message.content.match(setRegex) &&
-            (!(key in data) || data[key] !== message.channel.id)
-          ) {
-            data[key] = message.channel.id;
-            try {
-              await json_dump();
-            } catch (err) {
-              throw new Error(err);
+    key_gen(message.guild)
+        .then(
+            async (key) => {
+                if (message.member.hasPermission("MANAGE_CHANNELS")) {
+                    if (
+                        message.content.match(setRegex) &&
+                        (!(key in data) || data[key] !== message.channel.id)
+                    ) {
+                        data[key] = message.channel.id;
+                        try {
+                            await json_dump();
+                        } catch (err) {
+                            throw new Error(err);
+                        }
+                        await message.channel.send(
+                            `Set message channel to ${message.channel}!`
+                        );
+                    } else if (
+                        message.content.match(testRegex) &&
+                        key in data
+                    ) {
+                        await send_message(message.member);
+                    }
+                }
+            },
+            (reason) => {
+                console.error(reason);
+                throw 0;
             }
-            await message.channel.send(
-              `Set message channel to ${message.channel}!`
-            );
-          } else if (message.content.match(testRegex) && key in data) {
-            await send_message(message.member);
-          }
-        }
-      },
-      (reason) => {
-        console.error(reason);
-        throw 0;
-      }
-    )
-    .catch((reason) => {
-      if (reason != 0) {
-        console.error(`Failed: ${reason}`);
-      }
-    });
+        )
+        .catch((reason) => {
+            if (reason != 0) {
+                console.error(`Failed: ${reason}`);
+            }
+        });
 });
 
 client.login(process.env.TOKEN);
