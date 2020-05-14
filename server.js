09c705831574309435a3a6898cba41dae3ed0ae4
Rouge <code@r0uge.org>
2020-05-13 21:04:07 -0700

    Add message delete handler
    Should have feature parity to the python bot now.

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ var data = require("./data.json");
 
 const setRegex = /^!set/i;
 const testRegex = /^!test/i;
+const delRegex = /^!deltoggle/i;
 
 let key_gen = (guild) => {
     return new Promise((resolve, reject) => {
         if (guild.id) {
-            resolve(`${guild.id}_channel`);
+            resolve(`${guild.id}`);
         } else {
             reject(`Guild has no id`);
         }
@@ -44,12 +45,12 @@ client.once("ready", () => {
 
 let send_message = async (member) => {
     key_gen(member.guild).then(async (key) => {
-        if (key in data) {
+        if (key in data && data[key].channel) {
             client.channels
-                .fetch(data[key])
+                .fetch(data[key].channel)
                 .then(async (channel) => {
                     console.log(
-                        `Sending a message in: ${channel}, in guild: ${member.guild.name}`
+                        `Sending a message in: ${channel}, in guild: ${member.guild}`
                     );
                     await channel.send(
                         Math.floor(Math.random() * 2) == 0 ? "bruh" : "Bruh"
@@ -59,22 +60,32 @@ let send_message = async (member) => {
                     throw new Error(reason);
                 });
         } else {
-            throw new Error(`No channel ID set in server ${member.guild.name}`);
+            throw new Error(`No channel ID set in server ${member.guild}`);
         }
     });
 };
 
+client.on("guildMemberRemove", async (member) => {
+    send_message(member).catch((reason) => {
+        console.error(`Failed: ${reason}`);
+    });
+});
+
 client.on("guildCreate", async (guild) => {
-    console.log(`Added to Guild: ${guild.name}`);
+    console.log(`Added to Guild: ${guild}`);
     if (guild.systemChannel) {
         await guild.systemChannel.send(
             "Hi, thanks for adding BruhBot! Use `!set` to set which channel to send to."
         );
     }
+    data[key_gen(guild)] = { channel: null, delete_message: false };
+    await json_dump().catch((reason) => {
+        console.error(`Failed: ${reason}`);
+    });
 });
 
 client.on("guildDelete", async (guild) => {
-    console.log(`Removed from Guild: ${guild.name}`);
+    console.log(`Removed from Guild: ${guild}`);
     key_gen(guild)
         .then(async (key) => {
             delete data[key];
@@ -85,10 +96,24 @@ client.on("guildDelete", async (guild) => {
         });
 });
 
-client.on("guildMemberRemove", async (member) => {
-    send_message(member).catch((reason) => {
-        console.error(`Failed: ${reason}`);
-    });
+client.on("messageDelete", async (message) => {
+    if (data[await key_gen(message.guild)].delete_message) {
+        const audit = await message.guild.fetchAuditLogs({
+            type: 72,
+            limit: 10,
+        });
+        for (entry in Array.from(audit.entries)) {
+            if (entry.target.id == message.author.id && !message.author.bot) {
+                console.log(
+                    `Sending delete message in channel: ${message.channel} in guild: ${message.guild}`
+                );
+                await message.channel.send(
+                    `${message.author} had a bruh moment`
+                );
+                break;
+            }
+        }
+    }
 });
 
 client.on("message", async (message) => {
@@ -98,11 +123,12 @@ client.on("message", async (message) => {
             if (message.member.hasPermission("MANAGE_CHANNELS")) {
                 if (
                     message.content.match(setRegex) &&
-                    (!(key in data) || data[key] !== message.channel.id)
+                    (!data[key].channel ||
+                        data[key].channel !== message.channel.id)
                 ) {
                     data[key] = message.channel.id;
                     console.log(
-                        `Dumping json for: ${message.channel.name}, in Guild: ${message.guild.name}`
+                        `Dumping json for: ${message.channel}, in Guild: ${message.guild}`
                     );
                     json_dump()
                         .then(
@@ -113,8 +139,20 @@ client.on("message", async (message) => {
                         .catch((reason) => {
                             throw new Error(err);
                         });
-                } else if (message.content.match(testRegex) && key in data) {
+                } else if (message.content.match(testRegex)) {
                     await send_message(message.member);
+                } else if (message.content.match(delRegex)) {
+                    data[key].delete_message = !data[key].delete_message;
+                    console.log(
+                        `Toggled the delete message in: ${message.guild}`
+                    );
+                    json_dump().then(
+                        await message.channel.send(
+                            data[key].delete_message
+                                ? "Turned delete message on"
+                                : "Turned delete message off"
+                        )
+                    );
                 }
             }
         })
